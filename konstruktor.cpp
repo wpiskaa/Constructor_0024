be68ae9cd3e0d0be28209e22f436b156c05bb9a6
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 13:59:53 +0700

    tambah constructor overload untuk nama dan constructor lengkap serta method cetak di class Mahasiswa

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -25,3 +25,20 @@ mahasiswa ::mahasiswa(int iNim)
 {
     nim = iNim;
 }
+mahasiswa ::mahasiswa(string iNama)
+{
+    nama = iNama;
+}
+
+mahasiswa ::mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+}
+void mahasiswa::cetak()
+{
+    cout << endl
+         << "Nim =" << nim << endl;
+
+    cout << " Nama = " << nama << endl;
+}

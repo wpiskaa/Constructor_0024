57cff4b71ccb0f2fa4a2d746ef5514ba81bf93e5
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 14:36:34 +0700

    membuat class buku dan akses

--- latihan9.cpp
+++ latihan9.cpp
@@ -4,3 +4,15 @@ using namespace std;
 class Peminjam;
 class Petugas;
 class Admin;
+
+class Buku
+{
+private:
+    string judul;
+    bool dipinjam;
+
+public:
+    Buku(string j) : judul(j), dipinjam(false) {}
+    friend class Petugas;
+    friend class Admin;
+};

bdca37d357a06dcba4d4c936c8804d73304d0a0a
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 13:54:32 +0700

    tambah fungsi cetakData untuk menampilkan isi array di class Angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -27,3 +27,10 @@ angka::~angka()
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}

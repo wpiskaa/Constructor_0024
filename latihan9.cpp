9ca76b5e4d21c36973bc87ba05e6c178fce71554
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 14:44:55 +0700

    fix all

--- latihan9.cpp
+++ latihan9.cpp
@@ -5,8 +5,7 @@ class Peminjam;
 class Petugas;
 class Admin;
 
-class Buku
-{
+class Buku {
 private:
     string judul;
     bool dipinjam;
@@ -16,8 +15,8 @@ public:
     friend class Petugas;
     friend class Admin;
 };
-class Peminjam
-{
+
+class Peminjam {
 private:
     string nama;
     int totalPinjam;
@@ -28,61 +27,56 @@ public:
     friend class Admin;
 };
 
-class Petugas
-{
+class Petugas {
 private:
     string nama, level;
 
 public:
     Petugas(string n, string l) : nama(n), level(l) {}
 
-    void pinjam(Buku *b, Peminjam *p)
-    {
-        if (!b->dipinjam)
-        {
+    void pinjam(Buku* b, Peminjam* p) {
+        if (!b->dipinjam) {
             b->dipinjam = true;
             p->totalPinjam++;
             cout << p->nama << " meminjam " << b->judul << endl;
         }
     }
-    void kembali(Buku *b, Peminjam *p)
-    {
-        if (b->dipinjam)
-        {
+
+    void kembali(Buku* b, Peminjam* p) {
+        if (b->dipinjam) {
             b->dipinjam = false;
             p->totalPinjam--;
             cout << p->nama << " mengembalikan " << b->judul << endl;
         }
     }
+
     friend class Admin;
 };
 
 class Admin {
-    public:
-        void lihat(Peminjam* p, Buku* b) {
-            cout << "Statistik: " << p->nama << " meminjam "
-                 << p->totalPinjam << " buku. Status buku: "
-                 << (b->dipinjam ? "Dipinjam" : "Tersedia") << endl;
-        }
-        void ubahLevel(Petugas* t, string baru) {
-            t->level = baru;
-            cout << "Level petugas diubah ke " << baru << endl;
-        }
-    };
-    
-    int main() {
-        Buku b("Dasar C++");
-        Peminjam p("Piska");
-        Petugas t("Rika", "Normal");
-        Admin a;
-    
-        t.pinjam(&b, &p);
-        a.lihat(&p, &b);
-        t.kembali(&b, &p);
-        a.ubahLevel(&t, "Admin");
-    
-        return 0;
-    
-}    
+public:
+    void lihat(Peminjam* p, Buku* b) {
+        cout << "Statistik: " << p->nama << " meminjam "
+             << p->totalPinjam << " buku. Status buku: "
+             << (b->dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
 
+    void ubahLevel(Petugas* t, string baru) {
+        t->level = baru;
+        cout << "Level petugas diubah ke " << baru << endl;
+    }
+};
 
+int main() {
+    Buku b("Dasar C++");
+    Peminjam p("Piska");
+    Petugas t("Rika", "Normal");
+    Admin a;
+
+    t.pinjam(&b, &p);
+    a.lihat(&p, &b);
+    t.kembali(&b, &p);
+    a.ubahLevel(&t, "Admin");
+
+    return 0;
+}

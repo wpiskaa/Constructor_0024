2000cb794841eea4224816500cf5bf530688948e
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 13:58:57 +0700

    implementasi constructor default dan constructor dengan parameter nim di class Mahasiswa

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -15,3 +15,13 @@ public:
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+mahasiswa ::mahasiswa()
+{
+    nim = 0;
+    nama = "":
+}
+
+mahasiswa ::mahasiswa(int iNim)
+{
+    nim = iNim;
+}

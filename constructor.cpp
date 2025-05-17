4fa6d0a0e39eb2a6d9c41649bac0ef3d8b0a9bed
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 13:43:13 +0700

    tambahkan class Buku dengan method chaining setJudul

--- constructor.cpp
+++ constructor.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 class buku
+{
+    string judul;
+
+    public:
+    buku setJudul(string judul)
+    {
+        this ->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+}

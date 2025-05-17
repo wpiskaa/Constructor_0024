ba650c2f546010f686e516a7a5b51494c4c8b39b
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-17 13:58:12 +0700

    buat class Mahasiswa dengan multiple constructor dan method cetak

---
+++ konstruktor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    int nim;
+    string nama;
+
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
